39ef87566af42fd3f56485ef482fa7f667ffd462
Dana Costa <dcosta2@yahoo.com>
2016-12-30 21:18:56 -0500

    Start Menu

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+
+	public float fadeInTime;
+	
+	private Image fadePanel;
+	private Color currentColor = Color.white;
+	
+	// Use this for initialization
+	void Start () {
+		fadePanel = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.timeSinceLevelLoad < fadeInTime) {
+			// Fade In
+			float currentAlpha = 1f - (Time.timeSinceLevelLoad)/fadeInTime;
+			currentColor.a = currentAlpha;
+//			float alphaChange = Time.deltaTime/fadeInTime;
+//			currentColor.a -= alphaChange;
+//			Debug.Log(currentColor.a);
+			fadePanel.color = currentColor;
+		} else {
+			gameObject.SetActive (false);
+		}
+	}
+}
--- Assets/Scripts/SplashPanel.cs
+++ Assets/Scripts/SplashPanel.cs
@@ -11,6 +11,12 @@ public class SplashPanel : MonoBehaviour {
 	void Start () {
 		myImage.GetComponent<CanvasRenderer>().SetAlpha(0.001f);
 		subtitle.GetComponent<CanvasRenderer>().SetAlpha(0.001f);
+		Color visible = myImage.color;
+		visible.a = 1f;
+		myImage.color = visible;
+		visible = subtitle.color;
+		visible.a = 1f;
+		subtitle.color = visible;
 		myImage.CrossFadeAlpha(1.0f, fadeTime/2, false);
 		Invoke("subtitleFadeIn", fadeTime/2);
 	}
