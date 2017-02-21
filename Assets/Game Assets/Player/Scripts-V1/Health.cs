ae01208c3db932b44d85044e93fa92b96a4ea2bc
Evilmog <evilmog.tmo@gmail.com>
2017-02-20 19:10:52 -0500

    Added random respawning.

--- Assets/Game Assets/Player/Scripts-V1/Health.cs
+++ Assets/Game Assets/Player/Scripts-V1/Health.cs
@@ -14,6 +14,16 @@ public class Health : NetworkBehaviour {
 
 	public RectTransform healthBar;
 
+	private NetworkStartPosition[] spawnPoints;
+
+	void Start ()
+	{
+		if (isLocalPlayer)
+		{
+			spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+		}
+	}
+
 	public void TakeDamage(int amount)
 	{
 		if (!isServer)
@@ -22,13 +32,33 @@ public class Health : NetworkBehaviour {
 		currentHealth -= amount;
 		if (currentHealth <= 0)
 		{
-			currentHealth = 0;
-			Debug.Log("Dead!");
+			currentHealth = maxHealth;
+
+			// called on the Server, but invoked on the Clients
+			RpcRespawn();
 		}
 	}
 
 	void OnChangeHealth (int health)
 	{
 		healthBar.sizeDelta = new Vector2(health * starting_width, healthBar.sizeDelta.y);
+	}
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isLocalPlayer)
+		{
+			// Set the spawn point to origin as a default value
+			Vector3 spawnPoint = Vector3.zero;
+
+			// If there is a spawn point array and the array is not empty, pick one at random
+			if (spawnPoints != null && spawnPoints.Length > 0)
+			{
+				spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+			}
+
+			// Set the player’s position to the chosen spawn point
+			transform.position = spawnPoint;
+		}
 	}
 }
